b1ee15b210c649161c3e07d16454e7a97215fd6c
Mustafa Ali <mustafamire@gmail.com>
2021-11-09 21:40:22 +0000

    add div's for different sections of navabr & href

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,12 @@ import React from './navbar.scss'
 export default function Navbar() {
     return (
         <div className = "navbar">
+            <div className="wrapper">
+                <div className="left">
+                   <a href="intro"> Hello and Welcome</a>
+                </div>
+                <div className="right"></div>
+            </div>
             
         </div>
     )
