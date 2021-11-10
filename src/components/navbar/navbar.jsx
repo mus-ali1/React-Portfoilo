655c9accec737bfaff2199cdb647b43a60b506fa
Mustafa Ali <mustafamire@gmail.com>
2021-11-10 00:29:13 +0000

    deconstruct icons had hamburger drop-down

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,28 @@
 import React from './navbar.scss'
+import {Person, Email} from "@material-ui/icons"
 
 export default function Navbar() {
     return (
-        <div className = "navbar">
+        <div className = "navbar active">
             <div className="wrapper">
                 <div className="left">
-                   <a href="intro"> Hello and Welcome</a>
+                   <a href="intro" className= "logo">genius.</a>
+                   <div className="itemContainer">
+                   <Person className="icon"/>
+                   <span>+44 7404 617 287</span>
+                   </div>
+                   <div className="itemContainer">
+                   <Email className="icon"/>
+                   <span>mustafa@genius.com</span>
+                   </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className = "line1"></span>
+                        <span className = "line2"></span>
+                        <span className = "line3"></span>
+                        </div>
+                </div>
             </div>
             
         </div>
