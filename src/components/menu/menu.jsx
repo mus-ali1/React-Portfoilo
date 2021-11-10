c0c662c3859630f381921122b9f7325fa1dd2eda
Mustafa Ali <mustafamire@gmail.com>
2021-11-10 21:53:00 +0000

    add logic for menu-bar to disappear upon OnClick event + viceversa

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,8 +1,22 @@
 import React from './menu.scss'
 
-export default function menu() {
+export default function menu({menuOpen , setMenuOpen}) {
     return (
-        <div className= "menu">
+        <div className= {"menu "+(menuOpen && "active")}>
+            <ul>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#intro">Home</a>
+                </li>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
             
         </div>
     )
