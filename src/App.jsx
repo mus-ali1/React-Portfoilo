b9b39ee484307f5e6783b0f9df393971ca6949ea
Mustafa Ali <mustafamire@gmail.com>
2021-11-10 21:54:35 +0000

    import useState from react and add props to navbar and menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import Portfolio from "./components/portfolio/portfolio";
 import Projects from "./components/projects/projects";
 import Contact from "./components/contact/contact";
 import "./app.scss"
+import { useState } from "react";
+import Menu from "./components/menu/menu";
+
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className= "app">
-      <Navbar/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
