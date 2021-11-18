2f4d81256ad4b68292644fbbe83ff4931d8ccb80
Mustafa Ali <mustafamire@gmail.com>
2021-11-18 17:41:35 +0000

    add data for each project links / imgs etc

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -10,57 +10,62 @@ export default function Works() {
   const data = [
     {
       id: "1",
-      github: "https://github.com/mus-ali1/EmployeeTracker",
+      github: "https://github.com/mus-ali1/sharing-is-caring",
       deployed: "https://sharing-is-caring-dreamteam.herokuapp.com/login",
-      title: "Employee- Tracker",
+      title: "Full-Stack Application",
       desc:
-        "A command line application which utilizes node JS Express and Mysql to provide a useful and easily accessible employee management system. ",
-      img:
-        "./assets/employee-tracker.png",
-    },
-    {
-      id: "2",
-      icon: "./assets/github.png",
-      title: "Weather Dashboard",
-      desc:
-        "An application which fetchs data from an API which allows a user to submit and specify a particular location and then be presented with the weather for that location for the next 24 hours.",
-      img:
-        "./assets/weather.png",
-    },
-    {
-      id: "3",
-      icon: "./assets/github.png",
-      title: "Sharing is Caring",
-      desc:
-        "This is a Full- Stack Application which allows the users to add recipes based on their own culinary tradition, they can also comment on & vote for their favourite recipes.",
+        "Created in an NodeJS environment with a MYSQL back-end, Express-Server,Sequelize as the ORM, and Handlebars for templating. This app allows users to add and retrieve recipes.   ",
       img:
         "./assets/Screenshot.png",
     },
     {
-      id: "3",
-      icon: "./assets/github.png",
-      title: "Express Note-Taker",
+      id: "2",
+      github: "https://github.com/mus-ali1/NoSql-Workout-Tracker",
+      deployed: "https://mus-ali1fitness-tracker.herokuapp.com/",
+      title: "NoSql Workout Tracker",
       desc:
-        "This is a Full- Stack Application which allows the users to add recipes based on their own culinary tradition, they can also comment on & vote for their favourite recipes.",
+        "A workout tracker created using a Mongo database with a Mongoose schema and routes handled with Express. Allows users to track and update thier workouts.  ",
       img:
-        "./assets/express-noteTaker.png",
+        "./assets/appthree.png",
+     
     },
     {
       id: "3",
-      icon: "./assets/github.png",
-      title: "Readme Generator",
+      github: "https://github.com/mus-ali1/EmployeeTracker",
+      deployed: "https://www.youtube.com/watch?v=vC1KFeJ1dhM",
+      title: "Employee Management System",
       desc:
-        "This is a Full- Stack Application which allows the users to add recipes based on their own culinary tradition, they can also comment on & vote for their favourite recipes.",
+        " A command line application created in NodeJS environment uses Express to handle routes and MySql for the database. Allows employer to update records of employees as part of thier organization. ",
       img:
-        "./assets/ReadMeGenerator.png",
+        "./assets/employee-tracker.png",
+    },
+    {
+      id: "4",
+      github: "https://github.com/mus-ali1/E-Commerce-Back-End",
+      deployed: "https://www.youtube.com/watch?v=iEXJofg6HSs",
+      title: "E-Commerce Back-End",
+      desc:
+        " Developed using NodeJS ,the application uses Express.js API and Sequelize as an ORM to interact with a MySQL database, tested for end-points using Insomnia. The App provides full back-end functionality for an E-Commerce website.",
+      img:
+        "./assets/categories.png",
+    },
+    {
+      id: "5",
+      github: "https://github.com/mus-ali1/Note-Taker",
+      deployed: "https://musali-note-taker.herokuapp.com/",
+      title: "Express Note-Taker",
+      desc:
+        "Created using NodeJS, the app uses an Express.js back end and will save and retrieve user generated note data from a JSON file. Allows user to input notes which are then returned to the back-end",
+      img:
+        "./assets/express-noteTaker.png",
     },
   ];
 
   const handleClick = (way) => {
     way === "left"
     /// if currentSlide is greater then 0 (which is our first slide) decrease currentSlide by 1 , but if it is not greater than zero
-    //i.e it is our first slide then rather then reducing current slide by 1 (because we can't go any further) set currentSlide to 2 (the last item in our dataSet)
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+    //i.e it is our first slide then rather then reducing current slide by 1 (because we can't go any further) set currentSlide to 0 (the first item in our dataSet)
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 0 )
       /// if the current slide is less then the length of our data set - 1 (minus 1 because first array is indexed at zero, therefore dataSet is 3 but array is 2 so we have to -1 from dataSet), 
       //then we add 1 to our currentSlide (item) if currentSlide is no longer less than data.length -1 then return currentSlide to 0 i.e item zero in our array
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
@@ -84,7 +89,7 @@ export default function Works() {
                   <a href= {d.deployed} className= "social"><BsLink45Deg size={30}/></a>
                   </div>
                  <h2>{d.title}</h2>
-                  <p>{d.desc}</p>
+                  <p id="desc">{d.desc}</p>
               </div>
               </div>
               <div className="right">
