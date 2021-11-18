053c06208e3f46435a0ffbc6d0c7f71c1bb44c66
Mustafa Ali <mustafamire@gmail.com>
2021-11-18 18:13:17 +0000

    add image and form for submitting message

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,7 +3,20 @@ import React from './contact.scss'
 export default function contact() {
     return (
         <div className= "contact" id="contact">
-    
+      <div className="left">
+        <img src="./assets/shakepng.png" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit="">
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          </form>
         </div>
+       </div>   
     )
 }
+
+
+
