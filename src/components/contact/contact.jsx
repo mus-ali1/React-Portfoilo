01d33db79ec29773359f30768ce5c8fbcbd9c627
Mustafa Ali <mustafamire@gmail.com>
2021-11-19 02:34:31 +0000

    add useState to handle messages submitted in form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import React from './contact.scss'
 
-export default function contact() {
+
+export default function Contact() {
+   const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+
+  }
+
+
     return (
         <div className= "contact" id="contact">
       <div className="left">
@@ -8,10 +19,11 @@ export default function contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit="">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
           <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
           </form>
         </div>
        </div>   
